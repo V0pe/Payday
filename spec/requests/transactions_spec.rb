371a13a43b3b1f45f7f633c5312573b0d0f70161
V0pe <dampblew01@gmail.com>
2023-05-30 11:34:24 -0600

    add requests testing

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -17,13 +17,13 @@ RSpec.describe 'Categories', type: :request do
       )
     end
     let(:transaction) do
-        Transaction.create!(
-          name: 'transaction 1',
-          amount: 23,
-          user_id: user.id,
-          category_id = category.id
-          )
-      end
+      Transaction.create!(
+        name: 'transaction 1',
+        amount: 23,
+        user_id: user.id,
+        category_id: category.id
+      )
+    end
     before(:each) do
       user.save
       category.save
@@ -32,34 +32,34 @@ RSpec.describe 'Categories', type: :request do
     end
 
     it 'renders index template' do
-      get '/categories'
+      get "/categories/#{category.id}/transactions"
       expect(response).to render_template(:index)
     end
 
     it 'renders a successful response' do
-      get '/categories'
+      get "/categories/#{category.id}/transactions"
       expect(response).to be_successful
     end
 
-    it 'Test if Categories is loading correctly the body and have as title categories' do
-      get '/categories'
-      expect(response.body).to include('Categories')
+    it 'Test if transactions is loading correctly the body and have as transactions' do
+      get "/categories/#{category.id}/transactions"
+      expect(response.body).to include('Transactions')
     end
 
-    it 'Test if Categories is loading correctly the body and have the New inventory button' do
-      get '/categories'
-      expect(response.body).to include('add a new category')
+    it 'Test if page loading correctly the body and have the New button' do
+      get "/categories/#{category.id}/transactions"
+      expect(response.body).to include('New transaction')
     end
 
-    it 'Test if categories is loading correctly the body' do
-      get '/categories'
-      expect(response.body).to include('Category 1')
+    it 'Test if transactions is loading correctly the body' do
+      get "/categories/#{category.id}/transactions"
+      expect(response.body).to include('transaction 1')
     end
 
     it 'Test if inventory is the one that wecreated' do
-      get '/categories'
-      expect(response.body).to include('Category 1')
-      expect(response.body).to include('This is my category')
+      get "/categories/#{category.id}/transactions"
+      expect(response.body).to include('transaction 1')
+      expect(response.body).to include('23.0')
     end
   end
 end
